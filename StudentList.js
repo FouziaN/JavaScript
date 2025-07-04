0139ca1bc06ed1158700bb27d5a448e0a0dddef9
FouziaN <fouzianaaz09@gmail.com>
2025-07-04 10:43:32 +0530

    Array/Object

--- StudentList.js
+++ StudentList.js
@@ -47,14 +47,6 @@ function getTopper(arr) {
 
   console.table(getPassedStudents(students));
 
-
-  function hasPerfectScore(arr) {
-    return arr.some(student => student.marks === 100);
-  }
-
-  console.log("Has 100 scorer?", hasPerfectScore(students));
-
-
   function sortByMarksDesc(arr) {
     return [...arr].sort((a, b) => b.marks - a.marks);
   }
@@ -72,3 +64,74 @@ function getTopper(arr) {
   }
 
   console.log("Contains Rohan?", containsRohan(students));
+
+
+  let stringy = "Australia,Citizen";
+//   string = string.substring(5 , 8);
+//   console.log(string, string.length);
+  let arr = stringy.split(",")
+  console.log(arr);
+
+  let joinString = arr.join(",")
+  console.log(joinString)
+
+
+  const apiResponse = "aapl:200.1,amzn:200,cost:1000,dis:100";
+
+  class Stock{
+    constructor(name , price){
+        this.name = name;
+        this.price = price;
+    }
+
+    increasePrice(){
+        this.price = this.price + 1
+    }
+
+    companyGayi(){
+        this.price = 0;
+    }
+
+    assignCeo(ceoName){
+        this.ceo = ceoName
+    }
+  }
+
+  function processResponse(apiResponse){
+    let arr = apiResponse.split(",")
+    console.log(arr)
+    let arr2 = []
+    for(let i = 0 ; i < arr.length ; i++){
+        let val = arr[i] // val = "aapl:200"
+        let res = val.split(":") 
+        let newObj = new Stock(res[0] , parseFloat(res[1]))
+        arr2.push(newObj)
+    }
+    console.log(arr2)
+    return arr2
+  }
+  let stockPrices = processResponse(apiResponse)
+  stockPrices[1].increasePrice()
+  stockPrices[2].companyGayi()
+  stockPrices[1].assignCeo("Bezos")
+  console.log(stockPrices)
+
+  class Batsman{
+    constructor(name , matches , runs){
+        this.name = name;
+        this.matches = matches;
+        this.runs = runs;
+    }
+
+    calculateAvg(){
+        return (this.runs / this.matches);
+    }
+  }
+
+  let b1 = new Batsman("b1" , 1 , 60)
+  let b2 = new Batsman("b2" , 2 , 60)
+  let b3 = new Batsman("b3" , 3 , 60)
+  let b4 = new Batsman("b4" , 4 , 60)
+  
+ let average =  b3.calculateAvg()
+ console.log(average)
