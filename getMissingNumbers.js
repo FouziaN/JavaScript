a75c3994135dad2646b6e75957cb06a67073d894
FouziaN <fouzianaaz09@gmail.com>
2025-07-09 17:39:56 +0530

    MissingNumbers

--- getMissingNumbers.js
+++ getMissingNumbers.js
@@ -1,21 +1,47 @@
 // find the missing element
 
-let arr = [1 , 3 , 4 , 6]
+let arr = [1 , 112] 
 
+// Input = 1 , 112 
+// opp / Iteration = number of operations / 112
+// 2 operation = Outerloop
+// 110 operations = innerloop
+
+//[2, 5, 7, 8 , 9 ]
 function getMissingNumbers(arr){
-    let missingNumbers = []
-    for(let i = 0 ; i < 9 ; i++){
-        let count = 0;
-        for(let j = 0 ; j < arr.length ; j++){
-            if(i !== arr[j]){
-                count++;
-            }
+    let prev = arr[0]
+    let opp = 0
+    let missingElement = []
+    for(let i = 0 ; i < arr.length ; i++){
+        opp++
+        let curr = arr[i]
+        for(let j = prev + 1 ; j < curr ; j++){
+           missingElement.push(j)
+           opp++
         }
-        if (count === arr.length) {
-            missingNumbers.push(i);
-        }
+        prev = arr[i]
     }
-    return missingNumbers;
+    console.log(opp)
+    return missingElement
 }
 
 console.log(getMissingNumbers(arr))
+
+
+// function getMissingNumbers(arr){
+//     let missingNumbers = []
+//     for(let i = 0 ; i < 9 ; i++){
+//         let count = 0;
+//         for(let j = 0 ; j < arr.length ; j++){
+//             if(i !== arr[j]){
+//                 count++;
+//             }
+//         }
+//         if (count === arr.length) {
+//             missingNumbers.push(i);
+//         }
+//     }
+//     return missingNumbers;
+// }
+
+// console.log(getMissingNumbers(arr))
